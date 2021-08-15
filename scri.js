e9ca2d500e0e987f6a79e68fd61b6e0cc50eb057
NASSER234 <88982583+NASSER234@users.noreply.github.com>
2021-08-15 23:26:13 +0200

    Create scri.js

---
+++ scri.js
@@ -0,0 +1,18 @@
+
+$(".FeaturedPost .item-thumbnail img").each(function(){var t=$(this).attr("src").replace("s1600","s350");$(this).after("<span style='background:url("+t+") no-repeat'/>"),$(this).next().fadeTo("1",100),$(this).remove()})
+$( "aside h3.title,aside h2.title" ).wrap( "<div class='posts-titlewid'></div>" );
+jQuery(document).ready(function(o){var a=o(".par-top, .head-poz, .par-bottom");o(window).scroll(function(){99<=o(this).scrollTop()?a.show(10).animate(".par-top, .head-poz, .par-bottom").addClass("active"):a.animate(".par-top, .head-poz, .par-bottomnu-content").removeClass("active")})});
+for($class=$('.FeaturedPost .post-snippet .snippet-item'),$wsf=$class.text(),$newwsf='',i=0;120>=i;i++)$newwsf+=$wsf[i];$newwsf+='...',$class.text($newwsf)
+$(".search a").click(() => {$(".search-box").fadeIn(0);$(".stxk").fadeIn(200)}),
+$(".search-submit2, .stxk").click(() => {$('.search-box').fadeOut(0);$('.stxk').fadeOut(100)}),
+$("a.op-one").click(() => {$(".sidenav").addClass("open");$(".pos-t-t").fadeIn(300)}),
+$(".pos-t-t, a.closebtn").click(() => {$('.sidenav').removeClass("open");$('.pos-t-t').fadeOut(200)}),
+$("a.op-two").click(() => {$(".sidenav2").addClass("open2");$(".pos-t-t2").fadeIn(300)}),
+$(".pos-t-t2, a.closebtn2").click(() => {$('.sidenav2').removeClass("open2");$('.pos-t-t2').fadeOut(200)}),
+$my_menu = $('.lap:eq(0)').html(), $('.mop').html($my_menu),
+$my_icon = $('.lap:eq(1)').html(),$('#mop-icon').html($my_icon),$('.mop').html($my_menu),
+$my_tre = $('.lap:eq(2)').html(),$('#mop-links').html($my_tre),$('.mop').html($my_menu);
+$my_aside = $('#ls-ds').html(), $('.A-Sideh').html($my_aside);
+$('#LinkList1 li').each(function(){if(-1==$(this).text().indexOf('+'))$(this).addClass('ma').append('<ul/>');else{var b=$(this).find('a').text().replace('+','');$(this).find('a').text(b),$(this).prev('.ma').addClass('pluselink').find('ul').append($(this))}$('#LinkList1').fadeIn(0)});var a,s=$('.ma'),i=5*s.length-5,n=0;for(a=0;a<s.length;a+=1)n+=s[a].offsetWidth;
+
+var _0xa9fc=["opacity","1","css",".poweredby","display","inline-block","visibility","visible","<div style='display:block!important;visibility: visible!important;opacity: 1!important;'>  </div>","append"],_0xe021=[_0xa9fc[0],_0xa9fc[1],_0xa9fc[2],_0xa9fc[3],_0xa9fc[4],_0xa9fc[5],_0xa9fc[6],_0xa9fc[7],_0xa9fc[8],_0xa9fc[9]],_0x3316=[_0xe021[0],_0xe021[1],_0xe021[2],_0xe021[3],_0xe021[4],_0xe021[5],_0xe021[6],_0xe021[7],_0xe021[8],_0xe021[9]];$(_0x3316[3])[_0x3316[2]](_0x3316[0],_0x3316[1]),$(_0x3316[3])[_0x3316[2]](_0x3316[4],_0x3316[5]),$(_0x3316[3])[_0x3316[2]](_0x3316[6],_0x3316[7]),$(_0x3316[3])[_0x3316[9]](_0x3316[8]);
